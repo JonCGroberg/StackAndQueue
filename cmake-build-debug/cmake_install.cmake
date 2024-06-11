9dc04e584e2d824c9162cdd305b39ebe530dac17
Jonathan <jongroberg@gmail.com>
2024-06-11 19:54:51 -0400

    header file for stack

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/jonathangroberg/CLionProjects/Stack&Que
+# Install script for directory: /Users/jonathangroberg/CLionProjects/StackAndQueue
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/jonathangroberg/CLionProjects/Stack&Que/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/jonathangroberg/CLionProjects/StackAndQueue/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
